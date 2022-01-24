4fd06385366e8fbbaff90cc35cf75e540be3487a
François Romain <francoisromain@gmail.com>
2022-01-24 09:22:57 +0100

    Supprime postcss des déependances

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,13 +3,11 @@ const autoprefixer = require("autoprefixer");
 const cssnano = require("cssnano");
 
 const mode = process.env.NODE_ENV;
-const dev = mode === "development";
+const dev = mode === "local";
 
 const config = {
   plugins: [
-    // Some plugins, like postcss-nested, need to run before Tailwind,
     tailwindcss(),
-    // But others, like autoprefixer, need to run after,
     autoprefixer(),
     !dev &&
       cssnano({
