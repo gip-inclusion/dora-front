218ccdf55e9f287cc3d14fc30026984b246f9308
François Romain <francoisromain@gmail.com>
2022-02-10 19:16:04 +0100

    Ajoute storybook

---
+++ .storybook/main.js
@@ -0,0 +1,16 @@
+const preprocess = require("svelte-preprocess");
+
+module.exports = {
+  stories: [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)",
+  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  framework: "@storybook/svelte",
+  core: {
+    builder: "storybook-builder-vite",
+  },
+  svelteOptions: {
+    preprocess: [preprocess({ postcss: true })],
+  },
+};
