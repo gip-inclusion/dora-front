718a5f40be3f861693948c9b97ea57b35f8af559
François Romain <francoisromain@gmail.com>
2022-04-06 09:45:56 +0200

    feat(services): ajoute plusieurs thematiques

--- src/routes/services/form/_service-form-wrapper.svelte
+++ src/routes/services/form/_service-form-wrapper.svelte
@@ -160,7 +160,9 @@
       serviceSchema,
       { skipDependenciesCheck: true, noScroll: false }
     );
-
+    // debugger;
+    console.log(JSON.parse(JSON.stringify(validatedData)));
+    console.log("validatedData", validatedData, validatedData.categories);
     if (valid) {
       assert(service.slug);
 
