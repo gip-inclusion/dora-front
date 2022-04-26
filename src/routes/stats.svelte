a704f5b6ad5b64a6e3eaa32aa828d01dbdcfc632
Cedric Rossi <cedric@terramater.net>
2022-04-26 16:39:38 +0200

    chore: externalise les liens vers les embeds des stats

--- src/routes/stats.svelte
+++ src/routes/stats.svelte
@@ -1,6 +1,6 @@
 <script>
   import CG from "$lib/components/layout/centered-grid.svelte";
-
+  import { METABASE_EMBED_URL, PLAUSIBLE_EMBED_URL } from "$lib/env.js";
   function hR(id) {
     iFrameResize({}, id);
   }
@@ -23,7 +23,7 @@
   <iframe
     class="col-span-full"
     title="Dashboard Plausible"
-    src="https://plausible.io/share/dora.incubateur.net?auth=LQk4ORKTzLFyGJSBAl761&embed=true&theme=light&background=%23FFFFFF"
+    src={PLAUSIBLE_EMBED_URL}
     frameborder="0"
     allowtransparency
     on:load={hR("#plausibleIframe")}
@@ -35,7 +35,7 @@
   <iframe
     class="col-span-full"
     title="Dashboard Metabase"
-    src="https://metabase.dora.fabrique.social.gouv.fr/public/dashboard/9f65d982-fe2e-48f6-bb41-b829b89c29f9#bordered=false&titled=false"
+    src={METABASE_EMBED_URL}
     frameborder="0"
     allowtransparency
     on:load={hR("#metabaseIframe")}
