2a6eb3786bd479de94b548aef0f3e28ea61397e4
Francois Romain <francoisromain@gmail.com>
2022-06-14 14:54:40 +0200

    fix: corrige l'affichage des documents

--- src/lib/components/services/form/field-model.svelte
+++ src/lib/components/services/form/field-model.svelte
@@ -14,7 +14,7 @@
   let haveSameValue = false;
 
   function compare(a, b) {
-    if (type === "array") {
+    if (type === "array" || type === "files") {
       if (a === b) return true;
       if (a == null || b == null) return false;
       if (a.length !== b.length) return false;
