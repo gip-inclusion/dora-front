95b8e02c551d1fc5104469e0d25b01bff802f5c8
AlexandreCantin <cantin.alexandre@gmail.com>
2022-07-12 16:54:02 +0200

    feat(nps): ne plus afficher le formulaire après envoi

--- src/lib/components/tally-nps-popup.svelte
+++ src/lib/components/tally-nps-popup.svelte
@@ -1,12 +1,18 @@
 <script>
   import { onDestroy, onMount } from "svelte";
+  import { browser } from "$app/env";
 
   export let formId;
   export let timeout = 45000;
+  const localStorageKey = `popup_${formId}`;
+
   let timeoutFn;
 
   onMount(() => {
-    if (window.Tally);
+    if (window.Tally) window.Tally.closePopup(formId);
+    const hasAnsweredForm = localStorage.getItem(localStorageKey);
+
+    if (hasAnsweredForm) return;
 
     timeoutFn = setTimeout(() => {
       window.Tally.openPopup(formId, {
@@ -14,12 +20,17 @@
         width: 420,
         hideTitle: true,
         autoClose: 0,
+
+        // Inspiré par : https://tally.so/help/popup-forms#f089d95828ef4e86a728611ded726822
+        onSubmit: () => {
+          localStorage.setItem(localStorageKey, "1");
+        },
       });
     }, timeout);
   });
 
   onDestroy(() => {
-    window.Tally.closePopup(formId);
+    if (browser) window.Tally.closePopup(formId);
     clearTimeout(timeoutFn);
   });
 </script>
