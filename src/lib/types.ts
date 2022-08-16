db2aa3f8feb5e333299e9251194c3c98dc0cef53
AlexandreCantin <cantin.alexandre@gmail.com>
2022-08-16 16:20:39 +0200

    Remove eslint unused-var rules

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -4,25 +4,16 @@ export type Structure = {
   numServices: number;
 };
 
-// eslint considère à tort que les valeurs enum ne sont pas utilisées...
 export enum SERVICE_STATUSES {
-  // eslint-disable-next-line no-unused-vars
   DRAFT = "DRAFT",
-  // eslint-disable-next-line no-unused-vars
   SUGGESTION = "SUGGESTION",
-  // eslint-disable-next-line no-unused-vars
   PUBLISHED = "PUBLISHED",
-  // eslint-disable-next-line no-unused-vars
   ARCHIVED = "ARCHIVED",
 }
 
-// eslint considère à tort que les valeurs enum ne sont pas utilisées...
 export enum SERVICE_UPDATE_STATUS {
-  // eslint-disable-next-line no-unused-vars
   NOT_NEEDED = "NOT_NEEDED",
-  // eslint-disable-next-line no-unused-vars
   NEEDED = "NEEDED",
-  // eslint-disable-next-line no-unused-vars
   REQUIRED = "REQUIRED",
 }
 
