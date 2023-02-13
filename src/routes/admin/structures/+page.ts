243d04a696083afb46e89f36b0b56ea26b1a243d
Cedric Rossi <cedric.rossi@beta.gouv.fr>
2023-02-13 11:22:26 +0100

    WIP

--- src/routes/admin/structures/+page.ts
+++ src/routes/admin/structures/+page.ts
@@ -3,9 +3,10 @@ import { getStructuresOptions } from "$lib/requests/structures";
 import type { PageLoad } from "./$types";
 
 export const load: PageLoad = async () => {
-  // TODO: avoid serializing
-  const servicesOptions = await getServicesOptions();
-  const structuresOptions = await getStructuresOptions();
+  const [servicesOptions, structuresOptions] = await Promise.all([
+    getServicesOptions(),
+    getStructuresOptions(),
+  ]);
 
   return {
     title: "Structures | Administration | DORA",
