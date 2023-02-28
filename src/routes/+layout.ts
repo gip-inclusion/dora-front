8df31623744dd6255aa425056f73f44632a42fa3
Cedric Rossi <cedric.rossi@beta.gouv.fr>
2023-02-28 16:51:42 +0100

    Déconnecte l'utilisateur si son token est proche de son expiration

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,15 +1,53 @@
-import { userInfo, validateCredsAndFillUserInfo } from "$lib/utils/auth";
+import { browser } from "$app/environment";
+import {
+  disconnect,
+  userInfo,
+  validateCredsAndFillUserInfo,
+} from "$lib/utils/auth";
 import { redirect } from "@sveltejs/kit";
+import dayjs from "dayjs";
 import { get } from "svelte/store";
 import type { LayoutLoad } from "./$types";
 
 export const prerender = false;
-export const load: LayoutLoad = async ({ url }) => {
+
+function tokenWillExpireSoon(tokenExpirationString: string): boolean {
+  const tokenExpirationDate = dayjs(tokenExpirationString);
+  if (!tokenExpirationDate.isValid()) {
+    // Impossible de parser la date d'expiration -- on considère que
+    // le token est invalide
+    return true;
+  }
+  const minimalValidityDate = dayjs().add(1, "days");
+  return tokenExpirationDate.isBefore(minimalValidityDate);
+}
+
+export const load: LayoutLoad = async ({ url, depends }) => {
+  if (!browser) {
+    // On ne fait pas d'authentification coté serveur
+    // => on peut court-circuiter les vérifications ici
+    return {};
+  }
+
+  // On veut vérifier le token à chaque changement de page
+  depends(url.pathname);
+
   let currentUserInfo = get(userInfo);
   if (!currentUserInfo) {
     await validateCredsAndFillUserInfo();
     currentUserInfo = get(userInfo);
   }
+
+  // Si l'utilisateur est connecté, mais que son token expire dans moins de 24h,
+  // on force une deconnexion, afin qu'il récupère un token frais dès qu'il en aura besoin.
+  if (currentUserInfo && tokenWillExpireSoon(currentUserInfo.tokenExpiration)) {
+    // logout and reload page
+    disconnect();
+    window.location.reload();
+  }
+
+  // Si l'utilisateur existe mais n'est rattaché à aucune structure,
+  // on le force à se rattacher
   if (
     currentUserInfo &&
     !(
@@ -22,5 +60,6 @@ export const load: LayoutLoad = async ({ url }) => {
   ) {
     throw redirect(302, "/auth/rattachement");
   }
+
   return {};
 };
