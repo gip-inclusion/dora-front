8fab345502fbea15cc5fd77a737bcaa0d55d64d6
Cedric Rossi <cedric.rossi@beta.gouv.fr>
2023-06-22 17:10:38 +0200

    Ajoute un userId persistent

--- src/lib/utils/stats.js
+++ src/lib/utils/stats.js
@@ -1,11 +1,23 @@
 import { getApiURL } from "$lib/utils/api";
 import { token } from "$lib/utils/auth";
 import { get } from "svelte/store";
+import hexoid from "hexoid";
+
+const analyticsIdKey = "userHash";
+function getAnalyticsId() {
+  let analyticsId = localStorage.getItem(analyticsIdKey);
+  if (!analyticsId) {
+    analyticsId = hexoid(32)();
+    localStorage.setItem(analyticsIdKey, analyticsId);
+  }
+  return analyticsId;
+}
 
 export function logAnalyticsEvent(tag, path, params = {}) {
   const data = {
     tag,
     path,
+    userHash: getAnalyticsId(),
     ...params,
   };
   const currentToken = get(token);
