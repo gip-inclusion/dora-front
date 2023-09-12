777d4f5236693a9dd29ba21ea800679bdfc7d69c
Cedric Rossi <cedric.rossi@beta.gouv.fr>
2023-09-12 11:03:06 +0200

    Aligne les items de menu à droite.

--- src/routes/admin/structures/structure-modal.svelte
+++ src/routes/admin/structures/structure-modal.svelte
@@ -28,11 +28,13 @@
   on:close={() => (structureSlug = null)}
   bind:isOpen
   title={structure?.name}
-  width="small"
+  width="medium"
   overflow
 >
-  {#if structure}
-    <ModerationButtonMenu entity={structure} onRefresh={handleRefresh} />
-    <StructureContacts {structure} />
-  {/if}
+  <div class=" m-s8">
+    {#if structure}
+      <ModerationButtonMenu entity={structure} onRefresh={handleRefresh} />
+      <StructureContacts {structure} />
+    {/if}
+  </div>
 </Modal>
