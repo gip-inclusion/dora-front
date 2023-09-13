df631b7200a33333d10a1a23c5227b212d91aa91
Cedric Rossi <cedric.rossi@beta.gouv.fr>
2023-09-13 18:53:48 +0200

    Resolution d'avertissements de build

--- svelte.config.js
+++ svelte.config.js
@@ -53,11 +53,9 @@ const config = {
     },
   },
   vitePlugin: {
-    experimental: {
-      inspector: {
-        showToggleButton: "always",
-        toggleButtonPos: "bottom-right",
-      },
+    inspector: {
+      showToggleButton: "always",
+      toggleButtonPos: "bottom-right",
     },
   },
   onwarn(warning, defaultHandler) {
