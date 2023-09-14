8fec4868b7fef0dab8d9ca766400b807709220cc
AlexandreCantin <cantin.alexandre@gmail.com>
2023-09-14 16:16:33 +0200

    Modification de la phrase

--- src/routes/structures/[slug]/antennes/new-branch-modal.svelte
+++ src/routes/structures/[slug]/antennes/new-branch-modal.svelte
@@ -12,71 +12,28 @@
     title="Créer une ou plusieurs antennes"
     width="medium"
   >
-    <h2>Qu’est-ce qu’une antenne ?</h2>
-
     <p class="text-f16">
-      Une antenne est une <strong>nouvelle structure</strong> sur Dora
-      <strong>rattachée à votre structure principale</strong>. Une antenne a
-      ainsi la possibilité d’afficher <strong>ses propres offres</strong> et
-      d’être gérée par ses
-      <strong>propres administrateurs et collaborateurs</strong>. Une antenne
-      peut avoir le même numéro SIRET que la structure principale ou un numéro
-      de SIRET différent.
-    </p>
-
-    <h2 class="mt-s32">
-      Quand la création d’une antenne est-elle nécessaire ?
-    </h2>
-
-    <p class="text-f16">
-      Entre votre structure principale et votre(vos) antenne(s), les
-      collaborateurs qui diffusent des offres sur DORA sont :
-    </p>
-
-    <ul class="mt-s10 list-disc pl-s16 text-gray-text">
-      <li class="text-f16">
-        Différents : ✅ OUI, la création d’antenne est nécessaire
-      </li>
-      <li class="text-f16">
-        Les mêmes : ❌ NON, la création d’antenne n’est pas nécessaire et dans
-        ce cas, choisissez la simplicité 😉 : gérez toutes vos offres sur votre
-        structure principale en créant plusieurs services (un par localité par
-        exemple !).
-      </li>
-    </ul>
-
-    <h2 class="mt-s32">Comment créer une nouvelle antenne ?</h2>
-
-    <p class="mb-s20 text-f16">
       Pour demander la création d’une ou plusieurs antennes, il suffit
-      simplement de nous en faire la demande
-      <a
+      simplement de nous en faire la demande via notre chatbot (bouton “Discuter
+      avec nous” en bas de la page <a
         href="https://aide.dora.inclusion.beta.gouv.fr/fr/"
         class="text-magenta-cta underline"
         target="_blank"
         title="Ouverture dans une nouvelle fenêtre"
-        rel="noopener"
-      >
-        via notre chatbot
-      </a> (bouton "Discuter avec nous" en bas de la page).
-    </p>
-
-    <p
-      class="mb-s20 border-l-4 border-l-warning bg-warning-light p-s10 pl-s20 text-f16"
-    >
-      Vous aurez besoin de vous munir du nom de cette nouvelle structure et de
-      son numéro Siret
-    </p>
-
-    <p
-      class="mb-s20 border-l-4 border-l-success bg-success-light p-s10 pl-s20 text-f16"
-    >
-      Le numéro Siret peut être différent ou identique à la structure principale
+        rel="noopener">https://aide.dora.inclusion.beta.gouv.fr/fr/</a
+      >).
     </p>
 
     <p class="text-f16">
-      Une fois créée, vous retrouverez l’accès à <strong>vos structures</strong>
-      depuis votre tableau de bord principal.
+      Vous aurez besoin de vous munir du nom de cette nouvelle structure et du
+      lien vers la structure mère sur DORA.
+    </p>
+
+    <p class="text-f16">
+      Si votre antenne a un numéro SIRET différent de la structure mère, vous
+      pouvez directement créer l’antenne en cliquant sur le bouton "Adhérer à
+      une structure" (bouton se trouvant dans le menu déroulant de vos
+      structures d’appartenance - dans l’en-tête du site DORA).
     </p>
   </Modal>
 {/if}
