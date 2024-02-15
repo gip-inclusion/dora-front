993fe9d5ff9abcb24af979ab5a10c552f69607fc
Cedric Rossi <cedric.rossi@beta.gouv.fr>
2024-02-15 17:17:57 +0100

    Corrige le chargement de la modale

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,26 +12,29 @@
   import Modal from "$lib/components/hoc/modal.svelte";
 
   import type { PageData } from "./$types";
+  import { browser } from "$app/environment";
 
   export let data: PageData;
 
   let isVideoModalOpen = false;
 </script>
 
-<Modal bind:isOpen={isVideoModalOpen} title="Le formulaire d’orientation">
-  <div style="position: relative; padding-top: 50%;">
-    <iframe
-      title="DORA - Orienter un bénéficiaire"
-      width="100%"
-      height="100%"
-      src="https://tube.numerique.gouv.fr/videos/embed/c620b3c6-d9c8-46aa-8ab6-42b14b3f45a0?subtitle=fr&amp;warningTitle=0&amp;peertubeLink=0&amp;p2p=0"
-      frameborder="0"
-      allowfullscreen="true"
-      sandbox="allow-same-origin allow-scripts allow-popups"
-      style="position: absolute; inset: 0px;"
-    ></iframe>
-  </div>
-</Modal>
+{#if browser}
+  <Modal bind:isOpen={isVideoModalOpen} title="Le formulaire d’orientation">
+    <div style="position: relative; padding-top: 50%;">
+      <iframe
+        title="DORA - Orienter un bénéficiaire"
+        width="100%"
+        height="100%"
+        src="https://tube.numerique.gouv.fr/videos/embed/c620b3c6-d9c8-46aa-8ab6-42b14b3f45a0?subtitle=fr&amp;warningTitle=0&amp;peertubeLink=0&amp;p2p=0"
+        frameborder="0"
+        allowfullscreen
+        sandbox="allow-same-origin allow-scripts allow-popups"
+        style="position: absolute; inset: 0px;"
+      ></iframe>
+    </div>
+  </Modal>
+{/if}
 
 <CenteredGrid bgColor="bg-magenta-10 mb-s32">
   <h1 class="m-auto mb-s16 text-balance text-center text-france-blue">
@@ -78,14 +81,14 @@
         >
         vers la solution identifiée, le tout en
         <strong>moins de 5 minutes</strong>.
-      </p>
 
-      <Button
-        label="Voir la vidéo de démonstration"
-        on:click={() => (isVideoModalOpen = true)}
-        noBackground
-        noPadding
-      />
+        <Button
+          label="Voir la vidéo de démonstration"
+          on:click={() => (isVideoModalOpen = true)}
+          noBackground
+          noPadding
+        />
+      </p>
     </div>
     <div class="md:flex-1">
       <img src={illuMobiliser} alt="" class="mb-s16 w-full" />
