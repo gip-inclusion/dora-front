949a0073c0ae50426c5ecb3923b0c5ed3c9eacb0
Gérald Gounot <gerald@gounot.eu>
2024-04-01 17:07:49 +0200

    Renomme user-main-activity-form en user-onboarding-form

--- src/lib/components/user/user-onboarding-modal.svelte
+++ src/lib/components/user/user-onboarding-modal.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import UserMainActivityForm from "./user-main-activity-form.svelte";
+  import UserOnboardingForm from "./user-onboarding-form.svelte";
   import Modal from "$lib/components/hoc/modal.svelte";
 
   let isOpen = true;
@@ -16,5 +16,5 @@
   width="medium"
   canClose={false}
 >
-  <UserMainActivityForm {onSuccess} />
+  <UserOnboardingForm {onSuccess} />
 </Modal>
