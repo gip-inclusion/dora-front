47426d503fbdbb0d4fb9b10f240013f0c8727c62
Gérald Gounot <gerald@gounot.eu>
2024-04-23 16:37:10 +0200

    checkbox-mark: met à jour le commentaire explicatif

--- src/lib/components/display/checkbox-mark.svelte
+++ src/lib/components/display/checkbox-mark.svelte
@@ -12,7 +12,15 @@
 </div>
 
 <style>
-  /* Handle "checked state" with CSS when adjacent to checkbox input */
+  /* Ce symbole de case à cocher peut être coché de deux façons différente :
+   * - En mettant la prop checked à true ;
+   * - En plaçant le composant directement à la suite d'un <input type="checkbox" />.
+   *
+   * La deuxième façon utilise la déclaration CSS globale ci-dessous pour afficher la coche
+   * lorsque l'<input type="checkbox" /> qui précède ce composant est dans l'état « checked ».
+   *
+   * Il n'est pas nécessaire d'utiliser la prop checked avec la deuxième façon.
+   */
   :global(input[type="checkbox"]:checked + .checkbox .checkbox-mark) {
     @apply visible;
   }
