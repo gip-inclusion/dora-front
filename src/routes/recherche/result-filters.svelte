56b735911d5da66b8636b7c5b0d6abc7f551c836
Gérald Gounot <gerald@gounot.eu>
2024-04-24 14:05:19 +0200

    Ajout composant result-filters

---
+++ src/routes/recherche/result-filters.svelte
@@ -0,0 +1,37 @@
+<script lang="ts" context="module">
+  export interface Filters {
+    kinds: Array<string>;
+    feeConditions: Array<string>;
+    locationKinds: Array<string>;
+  }
+</script>
+
+<script lang="ts">
+  import type { ServicesOptions } from "$lib/types";
+
+  import ResultFilter from "./result-filter.svelte";
+
+  export let servicesOptions: ServicesOptions;
+  export let filters: Filters;
+</script>
+
+<div class="flex flex-col gap-s32">
+  <ResultFilter
+    id="kinds"
+    label="Type de service"
+    choices={servicesOptions.kinds}
+    bind:group={filters.kinds}
+  />
+  <ResultFilter
+    id="locationKinds"
+    label="Lieu d’accueil"
+    choices={servicesOptions.locationKinds}
+    bind:group={filters.locationKinds}
+  />
+  <ResultFilter
+    id="feeConditions"
+    label="Frais à charge"
+    choices={servicesOptions.feeConditions}
+    bind:group={filters.feeConditions}
+  />
+</div>
