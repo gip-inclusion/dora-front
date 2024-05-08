91312c57ea018897765691cdfd2ee7dd0c03b6b5
Gérald Gounot <gerald@gounot.eu>
2024-05-08 16:47:20 +0200

    Ajout du composant MapViewButton

---
+++ src/routes/recherche/map-view-button.svelte
@@ -0,0 +1,12 @@
+<script>
+  import mapViewImage from "$lib/assets/illustrations/map-view.jpg";
+
+  import Button from "$lib/components/display/button.svelte";
+</script>
+
+<div
+  style={`background-image: url('${mapViewImage}'); height: 116px`}
+  class="flex items-center justify-center rounded-ml bg-cover"
+>
+  <Button label="Voir sur la carte" />
+</div>
