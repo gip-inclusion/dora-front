05f5b1c9d2c469a60bf33ae9d7c978b514d56206
Frédéric Vergez <fred@ikarius.com>
2024-10-03 20:10:48 +0200

    Bouton ProConnect (minimal)

--- src/lib/components/specialized/pc-button.svelte
+++ src/lib/components/specialized/pc-button.svelte
@@ -1,28 +1,17 @@
 <script lang="ts">
-  import logoIC from "$lib/assets/inclusion-connect/logo-inclusion-connect.svg";
+  import { getApiURL } from "$lib/utils/api";
 
   export let nextPage: string;
-  export let loginHint: string = "";
 
- // href="https://localhost/oidc/login?next={encodeURIComponent(nextPage)}{loginHint}"
-
+  const loginUrl =
+    getApiURL() + "/oidc/login/?next=" + encodeURIComponent(nextPage);
 </script>
 
-
 <div class="text-center">
-  <a
-    class="mb-s24 inline-block"
-    href="https://localhost/oidc/login"
-  >
+  <a class="mb-s24 inline-block" href={loginUrl}>
     <div
-      class="mx-auto mt-s24 flex items-center justify-center rounded-full bg-france-blue p-s6 pr-s24 text-f16 text-white transition-colors hover:bg-[#2323FF]"
-    >
-      <span class="h-[62px] w-[62px] rounded-full bg-white p-s6">
-        <img src={logoIC} alt="" class="max-w-none" />
-      </span>
-
-        <div class="proconnect-button ml-s16 text-left"></div>
-    </div>
+      class="proconnect-button mx-auto mt-s24 flex items-center justify-center rounded-full bg-france-blue p-s6 pr-s24 text-f16 text-white transition-colors hover:bg-[#2323FF]"
+    ></div>
   </a>
 
   <div class="text-center">
