dfbacffafa8b8e932d0c5bd9161d053b69f63588
Frédéric Vergez <fred@ikarius.com>
2024-10-03 20:13:04 +0200

    OIDC callback
    Passe le token DRF et redirige vers le `next`

--- src/routes/auth/pc-callback/[token]/+page.ts
+++ src/routes/auth/pc-callback/[token]/+page.ts
@@ -1,13 +1,12 @@
 import { CANONICAL_URL } from "$lib/env";
 import { setToken } from "$lib/utils/auth";
 import { redirect } from "@sveltejs/kit";
+import { getNextPage } from "../../utils";
 
-export const load = ({ params }) => {
-	console.log("PC_CALLBACK");
-	
-	const token = params.token;
-	setToken(token);
+export const load = ({ params, url }) => {
+  const token = params.token;
+  setToken(token);
 
-	// home pour l'instant
-	redirect(302, CANONICAL_URL);
+  // home pour l'instant
+  redirect(302, CANONICAL_URL + getNextPage(url));
 };
