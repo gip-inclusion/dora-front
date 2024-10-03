4d628d6977c450f998da862b68a854b2e47c3ebe
Frédéric Vergez <fred@ikarius.com>
2024-10-03 20:17:17 +0200

    WIP callback d'identification

--- src/routes/auth/pc-callback/+page.ts
+++ src/routes/auth/pc-callback/+page.ts
@@ -11,9 +11,6 @@ export const ssr = false;
 export const load: PageLoad = async ({ url, parent }) => {
   await parent();
 
-
-  console.log("PC_CALLBACK");
-
   const forceLogin: boolean =
     url.searchParams.get("force_login") === "1" || false;
 
@@ -23,8 +20,7 @@ export const load: PageLoad = async ({ url, parent }) => {
     redirect(302, nextPage);
   }
 
-  // const targetUrl = `${getApiURL()}/oidc/login`;
-  const targetUrl = `https://localhost/oidc/login`;
+  const targetUrl = `${getApiURL()}/oidc/login`;
 
   // OIDC : problème, le params next_url ne peut être transmis
   // CORS: pourquoi ne pas faire directement le redirect depuis le back et gérer les CORS en front ?
@@ -37,8 +33,6 @@ export const load: PageLoad = async ({ url, parent }) => {
     },
     body: JSON.stringify({
       // eslint-disable-next-line camelcase
-      // redirect_uri: `${CANONICAL_URL}/auth/ic-callback?next=${encodeURIComponent(
-      // redirect_uri: `https://localhost/oidc/login`,
       loginHint: url.searchParams.get("login_hint") || undefined,
     }),
   });
