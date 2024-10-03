759174e46ebcffcc5ac23bb87bab5caeba3fd2da
Frédéric Vergez <fred@ikarius.com>
2024-10-03 20:12:19 +0200

    fixup

--- src/routes/auth/pc-logout/+page.ts
+++ src/routes/auth/pc-logout/+page.ts
@@ -1,9 +1,8 @@
-import { CANONICAL_URL } from "$lib/env";
-import { disconnect, setToken } from "$lib/utils/auth";
+import { getApiURL } from "$lib/utils/api";
+import { disconnect } from "$lib/utils/auth";
 import { redirect } from "@sveltejs/kit";
 
 export const load = () => {
-	console.log("PC_LOGOUT");
-	disconnect();
-	redirect(302, "https://localhost/oidc/pre_logout");
+  disconnect();
+  redirect(302, getApiURL() + "/oidc/pre_logout/");
 };
